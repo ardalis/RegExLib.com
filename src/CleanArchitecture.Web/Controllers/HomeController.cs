96840071bbeac860d1723292818509af6338e3df
Steve Smith <steve@kentsmiths.com>
2020-06-26 12:23:32 -0400

    Create razor page home page

--- src/CleanArchitecture.Web/Controllers/HomeController.cs
+++ src/CleanArchitecture.Web/Controllers/HomeController.cs
@@ -4,11 +4,6 @@ namespace RegExLib.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
