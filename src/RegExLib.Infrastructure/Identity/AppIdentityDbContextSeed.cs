5ff78d938b6b17bab9fd5e9005ccec2fb1d5bbb0
Steve Smith <steve@kentsmiths.com>
2020-06-26 13:54:55 -0400

    Adding Identity and seed stuff

---
+++ src/RegExLib.Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RegExLib.Core.Constants;
+using System.Threading.Tasks;
+
+namespace RegExLib.Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(AuthorizationConstants.Roles.ADMINISTRATORS));
+
+            string defaultUserName = "demouser@regexlib.test";
+            var defaultUser = new ApplicationUser { UserName = defaultUserName, Email = defaultUserName };
+            await userManager.CreateAsync(defaultUser, AuthorizationConstants.DEFAULT_PASSWORD);
+
+            string adminUserName = "admin@regexlib.test";
+            var adminUser = new ApplicationUser { UserName = adminUserName, Email = adminUserName };
+            await userManager.CreateAsync(adminUser, AuthorizationConstants.DEFAULT_PASSWORD);
+            adminUser = await userManager.FindByNameAsync(adminUserName);
+            await userManager.AddToRoleAsync(adminUser, AuthorizationConstants.Roles.ADMINISTRATORS);
+        }
+    }
+}
